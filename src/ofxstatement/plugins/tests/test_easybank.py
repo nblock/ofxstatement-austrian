e1f782f4330fc7951a5067025d5697a1986831a1
Raoul Bhatia <raoul@bhatia.at>
2019-08-08 20:57:55 +0200

    Fix Flake8 E741 ambiguous variable name 'l'

--- src/ofxstatement/plugins/tests/test_easybank.py
+++ src/ofxstatement/plugins/tests/test_easybank.py
@@ -32,28 +32,28 @@ class TestEasybankCreditCardCsvParser(unittest.TestCase):
             self.statement.end_date, datetime.datetime(2013, 7, 2, 0, 0))
 
     def test_line0_debit(self):
-        l = self.statement.lines[0]
-        self.assertEqual(l.amount, -5.99)
-        self.assertEqual(l.memo, "Some vendor/info")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 7, 2, 0, 0))
-        self.assertEqual(l.id, "12345678909876543212345")
+        line = self.statement.lines[0]
+        self.assertEqual(line.amount, -5.99)
+        self.assertEqual(line.memo, "Some vendor/info")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 7, 2, 0, 0))
+        self.assertEqual(line.id, "12345678909876543212345")
 
     def test_line1_credit(self):
-        l = self.statement.lines[1]
-        self.assertEqual(l.amount, 30.99)
-        self.assertEqual(l.memo, "Another vendor")
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 6, 21, 0, 0))
-        self.assertEqual(l.id, "23456789098765432123456")
+        line = self.statement.lines[1]
+        self.assertEqual(line.amount, 30.99)
+        self.assertEqual(line.memo, "Another vendor")
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 6, 21, 0, 0))
+        self.assertEqual(line.id, "23456789098765432123456")
 
     def test_line2_debit_with_foreign_currency(self):
-        l = self.statement.lines[2]
-        self.assertEqual(l.amount, -22.69)
-        self.assertEqual(l.memo, "Someone (GBP 22,89)")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 2, 19, 0, 0))
-        self.assertEqual(l.id, "34567890987654321234567")
+        line = self.statement.lines[2]
+        self.assertEqual(line.amount, -22.69)
+        self.assertEqual(line.memo, "Someone (GBP 22,89)")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 2, 19, 0, 0))
+        self.assertEqual(line.id, "34567890987654321234567")
 
 
 class TestEasybankGiroCsvParser(unittest.TestCase):
@@ -77,108 +77,108 @@ class TestEasybankGiroCsvParser(unittest.TestCase):
             self.statement.end_date, datetime.datetime(2015, 10, 7, 0, 0))
 
     def test_line0_interest_paid(self):
-        l = self.statement.lines[0]
-        self.assertEqual(l.check_no, "1")
-        self.assertEqual(l.amount, -0.42)
-        self.assertEqual(l.memo, "Einbehaltene KESt")
-        self.assertEqual(l.payee, l.memo)
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 1, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[0]
+        self.assertEqual(line.check_no, "1")
+        self.assertEqual(line.amount, -0.42)
+        self.assertEqual(line.memo, "Einbehaltene KESt")
+        self.assertEqual(line.payee, line.memo)
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 1, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line1_interest_earned(self):
-        l = self.statement.lines[1]
-        self.assertEqual(l.check_no, "2")
-        self.assertEqual(l.amount, 1.23)
-        self.assertEqual(l.memo, "Zinsen HABEN")
-        self.assertEqual(l.payee, l.memo)
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 1, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[1]
+        self.assertEqual(line.check_no, "2")
+        self.assertEqual(line.amount, 1.23)
+        self.assertEqual(line.memo, "Zinsen HABEN")
+        self.assertEqual(line.payee, line.memo)
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 1, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line2_debit_with_iban_bic(self):
-        l = self.statement.lines[2]
-        self.assertEqual(l.check_no, "3")
-        self.assertEqual(l.amount, -123.45)
-        self.assertEqual(l.memo, "Usage, specific reason")
+        line = self.statement.lines[2]
+        self.assertEqual(line.check_no, "3")
+        self.assertEqual(line.amount, -123.45)
+        self.assertEqual(line.memo, "Usage, specific reason")
         self.assertEqual(
-            l.payee, "Payment receiver (AT098765432109876543 ABCDEF1G235)")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 4, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+            line.payee, "Payment receiver (AT098765432109876543 ABCDEF1G235)")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 4, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line3_debit_legacy_bank_account(self):
-        l = self.statement.lines[3]
-        self.assertEqual(l.check_no, "4")
-        self.assertEqual(l.amount, -32)
-        self.assertEqual(l.memo, "Abbuchung Einzugsermächtigung")
-        self.assertEqual(l.payee, "Amazon *Mktplce EU-AT (01234567890 01234)")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 8, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[3]
+        self.assertEqual(line.check_no, "4")
+        self.assertEqual(line.amount, -32)
+        self.assertEqual(line.memo, "Abbuchung Einzugsermächtigung")
+        self.assertEqual(line.payee, "Amazon *Mktplce EU-AT (01234567890 01234)")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 8, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line4_debit_iban_only(self):
-        l = self.statement.lines[4]
-        self.assertEqual(l.check_no, "5")
-        self.assertEqual(l.amount, -1001.00)
-        self.assertEqual(l.memo, "CustomerNo: XXXXX OrderNr: YYYYYYYY")
-        self.assertEqual(l.payee, "Payment receiver (AT098765432109876543)")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 19, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[4]
+        self.assertEqual(line.check_no, "5")
+        self.assertEqual(line.amount, -1001.00)
+        self.assertEqual(line.memo, "CustomerNo: XXXXX OrderNr: YYYYYYYY")
+        self.assertEqual(line.payee, "Payment receiver (AT098765432109876543)")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 19, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line5_debit_withdraw(self):
-        l = self.statement.lines[5]
-        self.assertEqual(l.check_no, "6")
-        self.assertEqual(l.amount, -400)
-        self.assertEqual(l.memo, "Auszahlung Maestro 10.01")
-        self.assertEqual(l.payee, "AUTOMAT 01234567 K1 27.07.UM 18.57")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[5]
+        self.assertEqual(line.check_no, "6")
+        self.assertEqual(line.amount, -400)
+        self.assertEqual(line.memo, "Auszahlung Maestro 10.01")
+        self.assertEqual(line.payee, "AUTOMAT 01234567 K1 27.07.UM 18.57")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line6_debit_with_more_text_before_check_no(self):
-        l = self.statement.lines[6]
-        self.assertEqual(l.check_no, "7")
-        self.assertEqual(l.amount, -20.9)
-        self.assertEqual(l.memo, "XYZ INVOICE 01/14 123456789012345")
+        line = self.statement.lines[6]
+        self.assertEqual(line.check_no, "7")
+        self.assertEqual(line.amount, -20.9)
+        self.assertEqual(line.memo, "XYZ INVOICE 01/14 123456789012345")
         self.assertEqual(
-            l.payee,
+            line.payee,
             "Foobar XZY service AG (AT098765432109876543 ABCDEF1G235)")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 1, 31, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 1, 31, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line7_debit_with_more_text_before_check_no_and_without_banking_infos(self):    # NOQA
-        l = self.statement.lines[7]
-        self.assertEqual(l.check_no, "8")
-        self.assertEqual(l.amount, -8.4)
-        self.assertEqual(l.memo, "AT 8,40 DEBIT POS 18.0 5.14 10.08K1")
-        self.assertEqual(l.payee, "Somebody someony somewhere")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 2, 21, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[7]
+        self.assertEqual(line.check_no, "8")
+        self.assertEqual(line.amount, -8.4)
+        self.assertEqual(line.memo, "AT 8,40 DEBIT POS 18.0 5.14 10.08K1")
+        self.assertEqual(line.payee, "Somebody someony somewhere")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 2, 21, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line8_credit_iban_bic(self):
-        l = self.statement.lines[8]
-        self.assertEqual(l.check_no, "9")
-        self.assertEqual(l.amount, 12.1)
-        self.assertEqual(l.memo, "Gutschrift Überweisung")
+        line = self.statement.lines[8]
+        self.assertEqual(line.check_no, "9")
+        self.assertEqual(line.amount, 12.1)
+        self.assertEqual(line.memo, "Gutschrift Überweisung")
         self.assertEqual(
-            l.payee, "Some person (AT098765432109876543 ABCDEF1G235)")
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2014, 2, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+            line.payee, "Some person (AT098765432109876543 ABCDEF1G235)")
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2014, 2, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line9_memo_starts_with_forward_slash(self):
-        l = self.statement.lines[9]
-        self.assertEqual(l.check_no, "10")
-        self.assertEqual(l.amount, 9.98)
-        self.assertEqual(l.memo, "/INV/123456790 1.10.2015")
+        line = self.statement.lines[9]
+        self.assertEqual(line.check_no, "10")
+        self.assertEqual(line.amount, 9.98)
+        self.assertEqual(line.memo, "/INV/123456790 1.10.2015")
         self.assertEqual(
-            l.payee, "Some company (AT098765432109876543 ABCDEF12345)")
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2015, 10, 7, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+            line.payee, "Some company (AT098765432109876543 ABCDEF12345)")
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2015, 10, 7, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent autoindent
--- src/ofxstatement/plugins/tests/test_raiffeisen.py
+++ src/ofxstatement/plugins/tests/test_raiffeisen.py
@@ -30,64 +30,64 @@ class TestRaiffeisenCsvParser(unittest.TestCase):
             self.statement.end_date, datetime.datetime(2013, 7, 4, 0, 0))
 
     def test_line0_interest_earned(self):
-        l = self.statement.lines[0]
-        self.assertEqual(l.amount, 0.58)
-        self.assertEqual(l.memo, "0,125 % p.a. Habenzinsen ab 01.04.13")
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 6, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[0]
+        self.assertEqual(line.amount, 0.58)
+        self.assertEqual(line.memo, "0,125 % p.a. Habenzinsen ab 01.04.13")
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 6, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line1_interest_paid(self):
-        l = self.statement.lines[1]
-        self.assertEqual(l.amount, -0.15)
-        self.assertEqual(l.memo, "Kapitalertragsteuer")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 6, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[1]
+        self.assertEqual(line.amount, -0.15)
+        self.assertEqual(line.memo, "Kapitalertragsteuer")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 6, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line2_service_fee_print_statement(self):
-        l = self.statement.lines[2]
-        self.assertEqual(l.amount, -0.11)
-        self.assertEqual(l.memo, "Entgelt Kontoauszug")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 6, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[2]
+        self.assertEqual(line.amount, -0.11)
+        self.assertEqual(line.memo, "Entgelt Kontoauszug")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 6, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line3_service_fee(self):
-        l = self.statement.lines[3]
-        self.assertEqual(l.amount, -6.65)
-        self.assertEqual(l.memo, "Entgelt Kontoführung")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 6, 28, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        line = self.statement.lines[3]
+        self.assertEqual(line.amount, -6.65)
+        self.assertEqual(line.memo, "Entgelt Kontoführung")
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 6, 28, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line4_elba_payment(self):
-        l = self.statement.lines[4]
-        self.assertEqual(l.amount, -175.16)
+        line = self.statement.lines[4]
+        self.assertEqual(line.amount, -175.16)
         self.assertEqual(
-            l.memo, "ELBA-INTERNET VOM 29.06 UM 09:16 Empfänger: A person "
+            line.memo, "ELBA-INTERNET VOM 29.06 UM 09:16 Empfänger: A person "
             "Verwendungszweck: Invoice number 10")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 7, 1, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 7, 1, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line5_debit(self):
-        l = self.statement.lines[5]
-        self.assertEqual(l.amount, -100)
+        line = self.statement.lines[5]
+        self.assertEqual(line.amount, -100)
         self.assertEqual(
-            l.memo, "Lastschrift Auftraggeber: A company "
+            line.memo, "Lastschrift Auftraggeber: A company "
             "Kundendaten: 000000000000 111111111111 Verwendungszweck: reason")
-        self.assertEqual(l.trntype, "DEBIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 7, 1, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+        self.assertEqual(line.trntype, "DEBIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 7, 1, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
     def test_line6_credit(self):
-        l = self.statement.lines[6]
-        self.assertEqual(l.amount, 123.60)
+        line = self.statement.lines[6]
+        self.assertEqual(line.amount, 123.60)
         self.assertEqual(
-            l.memo, "Gutschrift Auftraggeber: A person Kundendaten: reason")
-        self.assertEqual(l.trntype, "CREDIT")
-        self.assertEqual(l.date, datetime.datetime(2013, 7, 4, 0, 0))
-        self.assertEqual(l.id, generate_transaction_id(l))
+            line.memo, "Gutschrift Auftraggeber: A person Kundendaten: reason")
+        self.assertEqual(line.trntype, "CREDIT")
+        self.assertEqual(line.date, datetime.datetime(2013, 7, 4, 0, 0))
+        self.assertEqual(line.id, generate_transaction_id(line))
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 smartindent autoindent
