269c6df2cf1383b312e9fe39c3da06a5ffe7f86e
Alexander Gerasiov <a@gerasiov.net>
2020-05-05 11:41:55 +0300

    Fix autotests for ofxstatement 0.6.4
    Signed-off-by: Alexander Gerasiov <a@gerasiov.net>

--- src/ofxstatement/plugins/tests/test_ingdiba.py
+++ src/ofxstatement/plugins/tests/test_ingdiba.py
@@ -3,6 +3,7 @@
 # See README.rst for more information.
 
 import datetime
+from decimal import Decimal
 import os
 import unittest
 from ofxstatement.statement import generate_transaction_id
@@ -21,8 +22,8 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_statement_properties(self):
         self.assertEqual(len(self.statement.lines), 6)
-        self.assertEqual(self.statement.start_balance, 0.0)
-        self.assertAlmostEqual(self.statement.end_balance, -992.03)
+        self.assertEqual(self.statement.start_balance, Decimal('0.0'))
+        self.assertEqual(self.statement.end_balance, Decimal('-992.03'))
         self.assertEqual(self.statement.currency, "EUR")
         self.assertEqual(self.statement.account_id, "12345678001")
         self.assertEqual(
@@ -32,7 +33,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line0_interest_earned(self):
         line = self.statement.lines[0]
-        self.assertEqual(line.amount, 12.23)
+        self.assertEqual(line.amount, Decimal('12.23'))
         self.assertEqual(line.memo, "Habenzinsen")
         self.assertEqual(line.trntype, "CREDIT")
         self.assertEqual(line.date, datetime.datetime(2013, 12, 31, 0, 0))
@@ -40,7 +41,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line1_interest_paid(self):
         line = self.statement.lines[1]
-        self.assertEqual(line.amount, -34.56)
+        self.assertEqual(line.amount, Decimal('-34.56'))
         self.assertEqual(line.memo, "Kapitalertragsteuer")
         self.assertEqual(line.trntype, "DEBIT")
         self.assertEqual(line.date, datetime.datetime(2013, 12, 31, 0, 0))
@@ -48,7 +49,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line2_credit(self):
         line = self.statement.lines[2]
-        self.assertEqual(line.amount, 500.00)
+        self.assertEqual(line.amount, Decimal('500.00'))
         self.assertEqual(line.memo, "Eingang: XXX")
         self.assertEqual(line.trntype, "CREDIT")
         self.assertEqual(line.date, datetime.datetime(2013, 12, 23, 0, 0))
@@ -56,7 +57,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line3_transfer_credit(self):
         line = self.statement.lines[3]
-        self.assertEqual(line.amount, 20.30)
+        self.assertEqual(line.amount, Decimal('20.30'))
         self.assertEqual(line.memo, "Umbuchung von 003")
         self.assertEqual(line.trntype, "CREDIT")
         self.assertEqual(line.date, datetime.datetime(2013, 9, 25, 0, 0))
@@ -64,7 +65,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line4_debit(self):
         line = self.statement.lines[4]
-        self.assertEqual(line.amount, -1500)
+        self.assertEqual(line.amount, Decimal('-1500'))
         self.assertEqual(line.memo, "Auszahlung - XXX")
         self.assertEqual(line.trntype, "DEBIT")
         self.assertEqual(line.date, datetime.datetime(2013, 8, 27, 0, 0))
@@ -72,7 +73,7 @@ class TestLivebankCsvParser(unittest.TestCase):
 
     def test_line5_bonus(self):
         line = self.statement.lines[5]
-        self.assertEqual(line.amount, 10.00)
+        self.assertEqual(line.amount, Decimal('10.00'))
         self.assertEqual(line.memo, "Prämie Foo")
         self.assertEqual(line.trntype, "CREDIT")
         self.assertEqual(line.date, datetime.datetime(2013, 8, 13, 0, 0))
